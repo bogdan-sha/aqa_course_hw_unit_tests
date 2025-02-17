d793e499b9eb86ef75b8ef25d089618a37bdb2b7
Bahdan Shamukou <bahdanshamukou@Bahdan-Shamukou.local>
2025-02-18 00:13:20 +0100

    HW-4: Solve Task 1, Task 2 and Task 3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,26 @@
   преобразовываясь в number
 
 */
+
+let minAge, maxAge, age;
+minAge = 18;
+maxAge = 60;
+age = '19';
+
+if (typeof age === 'string') {
+  age = parseInt(age);
+}
+
+if (typeof age !== 'number') {
+  console.log('Incorrect data type');
+} else {
+  if (age < minAge) {
+    console.log(`"You don't have access cause your age is ${age} It's less then "`);
+  } else if (age >= minAge && age < maxAge) {
+    console.log("Welcome  !");
+  } else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical work");
+  }
+}
