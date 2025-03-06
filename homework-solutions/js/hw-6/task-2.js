14f7f690342c14ceaa711eea3eaa292d8380520e
Bahdan Shamukou <mr.shamukov@gmail.com>
2025-03-06 16:07:57 +0100

    HW-6: Solve Task 1, Task 2 and Task 3

--- homework-solutions/js/hw-6/task-2.js
+++ homework-solutions/js/hw-6/task-2.js
@@ -13,7 +13,33 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
+let resultUnique = [];
 let resultNull;
+
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const competitorPizzasLower = competitorPizzas.map(pizza => pizza.toLowerCase());
+
+for (let myPizza of myPizzasT1) {
+  const myPizzaLower = myPizza.toLowerCase();
+  if (!competitorPizzasLower.includes(myPizzaLower)) {
+    resultUnique.push(myPizzaLower);
+  }
+  if (!resultUnique.length) {
+    resultNull = null;
+  }
+}
+
+for (let myPizza of myPizzasT2) {
+  const myPizzaLower = myPizza.toLowerCase();
+  if (!competitorPizzasLower.includes(myPizzaLower)) {
+    resultUnique.push(myPizzaLower);
+  }
+  if (!resultUnique.length) {
+    resultNull = null;
+  }
+}
+
 
 export { resultNull, resultUnique };
