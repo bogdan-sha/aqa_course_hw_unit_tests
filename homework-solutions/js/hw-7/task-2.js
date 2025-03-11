c0a3620d70525e8b29efed21f0a3e199f5619779
Bogdan <bogdan@Kates-MacBook-Pro.local>
2025-03-11 17:52:42 +0100

    HW-7: solve all tasks

--- homework-solutions/js/hw-7/task-2.js
+++ homework-solutions/js/hw-7/task-2.js
@@ -3,9 +3,20 @@
  Написать функцию, которая принимает на вход слово и проверяет, является ли это слово палиндромом
 */
 
+let word = 'racecar'
+
 function isPalindrom(word) {
-  // Ваш код
+  if (typeof word !== 'string') return false;
+
+  let palindrom = word.split('').filter(el => el !== '').reverse().join('');
+  if (word.toLowerCase() === palindrom.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(isPalindrom(word));
 
 /*
  2. findLongestWords()
@@ -15,7 +26,23 @@ function isPalindrom(word) {
 */
 
 function findLongestWords(sentence) {
-  // Ваш код
+  if (typeof sentence !== 'string') return sentence = [];
+
+  let array = sentence.split(' ').filter(el => el !== '');
+  let result = [];
+  let sentenceLength = 0;
+  
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > sentenceLength) {
+      sentenceLength = array[i].length;
+      result = [array[i]]; 
+    }
+    else if (array[i].length === sentenceLength) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
 }
 
 export { isPalindrom, findLongestWords };
